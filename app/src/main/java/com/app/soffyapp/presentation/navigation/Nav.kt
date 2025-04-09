07621347a6bb60c2368f2ca5919afe6f890fb3aa
ojavier <ojvilleda6@gmail.com>
2025-04-09 09:35:32 -0600

    Creación del archivo de navegación

--- app/src/main/java/com/app/soffyapp/presentation/navigation/Nav.kt
+++ app/src/main/java/com/app/soffyapp/presentation/navigation/Nav.kt
@@ -1,2 +1,28 @@
 package com.app.soffyapp.presentation.navigation
 
+// presentation/navigation/Nav.kt
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        topBar = { TopAppBar() }, // O bottomBar = { BottomNavBar() }
+        content = { padding ->
+            NavHost(
+                navController = navController,
+                startDestination = Screens.Home.route,
+                modifier = Modifier.padding(padding)
+            ) {
+                // Define tus pantallas aquí
+                composable(Screens.Home.route) { HomeScreen(navController) }
+                composable(Screens.Detail.route) { DetailScreen(navController) }
+            }
+        }
+    )
+}
+
+// Define un sealed class para las pantallas
+sealed class Screens(val route: String) {
+    object Home : Screens("home")
+    object Detail : Screens("detail")
+}
