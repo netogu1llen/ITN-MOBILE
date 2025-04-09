4f7e77822cfe1e3e8e407ebe1c8d30ff5f5b3454
ojavier <ojvilleda6@gmail.com>
2025-04-09 09:58:16 -0600

    Actualizar Main Activity

--- app/src/main/java/com/app/soffyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/app/soffyapp/presentation/MainActivity.kt
@@ -1,4 +1,14 @@
 package com.app.soffyapp.presentation
 
-class MainActivity {
+import com.app.soffyapp.presentation.navigation.AppNavigation
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyAppTheme {
+                AppNavigation()
+            }
+        }
+    }
 }
