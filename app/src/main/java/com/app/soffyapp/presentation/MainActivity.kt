812f265964d248e615f716b0abe82483841e38b3
ojavier <ojvilleda6@gmail.com>
2025-04-09 10:36:18 -0600

    Implementar librerias MainActivity.kt

--- app/src/main/java/com/app/soffyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/app/soffyapp/presentation/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.app.soffyapp.presentation
 
 import com.app.soffyapp.presentation.navigation.AppNavigation
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
