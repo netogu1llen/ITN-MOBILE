4eb2688e70dcaf20c552a5f9637fcb599c1dea16
ojavier <ojvilleda6@gmail.com>
2025-04-22 22:47:16 -0600

    Inicializar GoogleSignInOptions

--- app/src/main/java/com/app/soffyapp/presentation/screens/login.components/LoginScreen.kt
+++ app/src/main/java/com/app/soffyapp/presentation/screens/login.components/LoginScreen.kt
@@ -1,7 +1,6 @@
 package com.app.soffyapp.presentation.screens.login.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -11,10 +10,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.app.soffyapp.R
 
